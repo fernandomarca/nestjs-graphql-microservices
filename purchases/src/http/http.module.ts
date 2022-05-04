0dda82d6fbfbfe4de9ee405a9b7b8b8743fadc27
Fernando Marca Magalhães <fernandomarca@hotmail.com>
2022-05-04 12:43:28 -0300

    setup

---
+++ purchases/src/http/http.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from '../database/database.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver } from '@nestjs/apollo';
+import path from 'path';
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    GraphQLModule.forRoot({
+      driver: ApolloDriver,
+      autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
+  providers: [],
+  controllers: [],
+})
+export class HttpModule {}
